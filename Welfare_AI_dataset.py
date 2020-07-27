35bcbb96e71c9b6c30b4cc87b8e0e5853fbebe84
Yasminekaroui <63373107+Yasminekaroui@users.noreply.github.com>
2020-07-27 16:26:58 +0200

    Add files via upload

--- Welfare_AI_dataset.py
+++ Welfare_AI_dataset.py
@@ -43,10 +43,39 @@ class CowsDataset:
         dictionary = dictionary.to_numpy()
         if self.sheet_names is None:
             self.sheet_names = dictionary[:, 0]
-        df = [pd.DataFrame(pd.read_excel(DATASET_PATH, sheet).iloc[:, 2:34]) for
+        df = [pd.DataFrame(pd.read_excel(DATASET_PATH, sheet).iloc[:, 0:34]) for
               x, sheet in enumerate(self.sheet_names)]
         return df
+    
+    
+    def remove_steps_columns(self):
+        """
+        
 
+        Returns dataframe without the first 2 columns that indicate the begin of 
+        front and back step
+        -------
+        None.
+
+        """
+        df = [df.iloc[:, 2:34] for i, df in enumerate(self.df)]  
+        
+        return df
+              
+    
+   
+    def get_head(self, n_rows):
+        """
+        
+
+        Returns dataframe with the values of n_rows first rows
+        -------
+        None.
+
+        """
+        df = [df.head(n_rows).values for i, df in enumerate(self.df)]
+        return df
+    
     @staticmethod
     def get_side_sheets(side):
         """
@@ -162,7 +191,7 @@ class CowsDataset:
 
         """
         column = self.df[sheet_index][joint_name]
-        indexes = CowsDataset.get_extrema_indexes(column,order)
+        indexes = CowsDataset.get_extrema_indexes(column, order)
         sequences = np.split(column.to_numpy(), indexes)
         new_sequences = []
         for i, seq in enumerate(sequences):
@@ -170,17 +199,79 @@ class CowsDataset:
             seq = seq.mask(~seq.between(seq.quantile(number), seq.quantile(1-number)),np.nan)
             seq = seq.interpolate().to_numpy() 
             new_sequences.append(seq)
-        new_column = pd.Series(np.concatenate( new_sequences, axis=0 ))
+        new_column = pd.Series(np.concatenate(new_sequences, axis=0))
         new_column = new_column.interpolate()
         return new_column
             
-    def clean_dataset(self, number, order):
-        for i, sheet in enumerate(self.df):
-            columns = CowsDataset.get_joint_names(self.df)
-            for j, joint_name in enumerate(columns):
-                column = self.remove_outliers(i,joint_name, number, order)
-        return self.df
-       
+    def clean_dataset(self,index, number=0.05, order=10):
+        columns = CowsDataset.get_joint_names(self.df)
+        result = pd.DataFrame()
+        for j, joint_name in enumerate(columns):
+            column = self.remove_outliers(index,joint_name, number, order)
+            result = pd.concat([result, column], axis=1, sort=False)
+        result.columns = columns
+        return result
+    
+    
+    @staticmethod
+    def sliding_window(dataset, side):
+        """
+        
+
+        Parameters
+        ----------
+        dataset : TYPE
+            DESCRIPTION.
+
+        Returns a part of the dataset with the largest sliding window(between
+        1st front-step and 3rd back-step)
+                                                                   
+        -------
+        None.
+
+        """
+        periodsf = []
+        periodsb = []
+        for i, cow in enumerate(dataset):
+            df1 = cow['Front_Step'].to_frame().dropna().iloc[:, 0].astype(str).str[0].to_frame().astype(
+                int)  # take the first caracter
+            test = df1.iloc[0, 0] == 1  # test the begin of the first step
+            df1 = df1.diff()
+            if test:
+                df1.iloc[0] = 1
+        
+            periodsf.append(np.array(df1.where(df1 == 1).dropna().index + 2))#indexes of the begin of each front step
+            #of first step
+            
+            df2 = cow['Back_Step'].to_frame().dropna().iloc[:, 0].astype(str).str[0].to_frame().astype(
+                int)  # take the first caracter
+            test = df2.iloc[0, 0] == 1  # test the begin of the first step
+            df2 = df2.diff()
+            if test:
+                df2.iloc[0] = 1
+            periodsb.append(np.array(df2.where(df2 == 1).dropna().index + 2))#indexes of the begin of each front step
+        periodsf  = np.array(periodsf).T   
+        periodsb  = np.array(periodsb).T 
+        combined = np.vstack((periodsf, periodsb)).T
+        steps = []
+        for i in range(9):
+            steps.append(np.sort(np.concatenate(combined[i, :])))
+        third_bstep = []
+        for i, cow_steps in enumerate(steps):
+            third_bstep.append(cow_steps[5])#list of the begin of the third back step for each cow
+        window_size_side2 = max(third_bstep)
+        if (side=='side2'):
+            return window_size_side2
+        else:
+            third_bstep.remove(window_size_side2)
+            window_size_side1 =  max(third_bstep)
+            return window_size_side1
+    
+    #define new dataset with window_size rows
+    # dataset = pd.DataFrame(dataset.remove_steps_columns())
+    # train_dataset = dataset.head(window_size)
+        
+           
                 
         
           
